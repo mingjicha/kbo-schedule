a2e9aed56b31ab42d606bdd3d1028af2e623d6a7
Mungzi <mingi9100@naver.com>
2026-05-06 15:57:31 +0900

    https 수정

--- server.js
+++ server.js
@@ -763,5 +763,5 @@ app.get('/api/weather', async (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at https://localhost:${PORT}`);
 });
